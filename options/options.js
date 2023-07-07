e9f9c2c9c4be8b663564c4b7065989bb0f4dcd09
Crowdbotics <team@crowdbotics.com>
2023-07-07 17:41:53 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "the-park-space-41983",
+  url: "https://the-park-space-41983.botics.co",
+  api: "https://the-park-space-41983.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
